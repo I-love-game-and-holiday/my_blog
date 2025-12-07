76cdc1d5ad972a9985ec6b979f7ef0fd6b05ba69
I-love-game-and-holiday <134291981+I-love-game-and-holiday@users.noreply.github.com>
2025-12-07 11:20:53 +0900

    爆破前保存

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,25 @@
 import type { Metadata } from "next";
 import "nextra-theme-blog/style.css";
+import { NextraTheme } from './_components/nextra-theme'
+import type { FC, ReactNode } from 'react'
+import { Head } from 'nextra/components'
+import { getPageMap } from 'nextra/page-map'
 
 export const metadata: Metadata = {
   title: "技術ブログ",
   description: "技術系ブログ",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout: FC<{ children: ReactNode }> = async ({ children }) => {
+  const pageMap = await getPageMap()
   return (
-    <html lang="ja">
-      <body>
-        {children}
+    <html lang="ja" dir="ltr">
+      <Head faviconGlyph="✦" />
+      <body style={{ margin: 0 }}>
+        <NextraTheme pageMap={pageMap}>{children}</NextraTheme>
       </body>
     </html>
-  );
+  )
 }
+
+export default RootLayout
