709246c99ca2796f705c55ac51ba58ed30e76002
I-love-game-and-holiday <134291981+I-love-game-and-holiday@users.noreply.github.com>
2026-02-11 11:28:51 +0900

    規約違反部分の修正

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 
 export default function AboutPage() {
     return (
-        <div className="container py-16 md:py-24">
+        <div className="container py-section-static">
             <div className={`${CONTENT_MAX_WIDTH} mx-auto`}>
                 <h1 className="text-3xl font-bold mb-8">About</h1>
 
